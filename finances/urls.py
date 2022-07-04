16053436c8754516dc896371d55abda4a888639e
SanyaNanda <sanya.nanda@gmail.com>
2022-07-05 02:18:38 +0530

    dashboard and filters

--- finances/urls.py
+++ finances/urls.py
@@ -7,11 +7,13 @@ from .views import *
 app_name = 'financeS'
 
 urlpatterns = [
-   path('home/<username>/', HomeView,name="home"),
+   path('home/<username>/', DashboardView,name="home"),
    path('add-income/<username>/', AddIncomeView,name="add-income"),
    path('add-expense/<username>/', AddExpenseView,name="add-expense"),
    path('add-income-category/<username>/', AddIncomeCategoryView,name="add-income-category"),
    path('add-expense-category/<username>/', AddExpenseCategoryView,name="add-expense-category"),
    path('dashboard/<username>/', DashboardView,name="dashboard"),
+   path('expense/<username>/', ExpenseView,name="expense"),
+   path('income/<username>/', IncomeView,name="income"),
 
 ]
--- finances/views.py
+++ finances/views.py
@@ -1,7 +1,9 @@
 from calendar import month
+from nis import cat
 from django.shortcuts import render
 from django.shortcuts import  redirect
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from .models import Income, Expense, IncomeCategory, ExpenseCategory
 from django.contrib.auth.decorators import login_required
 import datetime
@@ -89,22 +91,20 @@ def AddIncomeCategoryView(request, username=None):
 def DashboardView(request, username=None):
     profile = User.objects.get(username=username)
     today = datetime.date.today()
-    # month = today.month-1
-    # year = today.year
+    month = today.month
+    year = today.year
+    print(request.GET)
     if 'month' in request.GET and 'year' in request.GET:
         month = request.GET['month']
         year = request.GET['year']
-    # else:
-    #     month = today.month-1
-    #     year = today.year
-        # if month!=None and year!=None:
-        #     month = today.month
-        #     year = today.year
-    month = today.month-1
-    year = today.year
-    print(month)
     monthly_income = Income.objects.filter(profile=profile, time_stamp__month=month, time_stamp__year=year).aggregate(Sum('amount'))['amount__sum']
+    monthly_income_details = Income.objects.filter(profile=profile, time_stamp__month=month, time_stamp__year=year)
     monthly_expense = Expense.objects.filter(profile=profile, time_stamp__month=month, time_stamp__year=year).aggregate(Sum('amount'))['amount__sum']
+    monthly_expense_details = Expense.objects.filter(profile=profile, time_stamp__month=month, time_stamp__year=year)
+    monthly_saving = monthly_income - monthly_expense
+    expense_percent = round(monthly_expense*100/monthly_income,2)
+    saving_percent = round(monthly_saving*100/monthly_income, 2)
+
     income_category = IncomeCategory.objects.filter(profile=profile)
     monthly_category_wise_income = {}
     monthly_category_wise_income_percent = {}
@@ -114,17 +114,18 @@ def DashboardView(request, username=None):
         monthly_category_wise_income[i.name] = income
         if income!=None:
             monthly_category_wise_income_percent[i.name] = round((income/monthly_income)*100, 2)
-    monthly_saving = monthly_income - monthly_expense
 
     expense_category = ExpenseCategory.objects.filter(profile=profile)
     monthly_category_wise_expense = {}   
     monthly_category_wise_expense_percent = {}
+    monthly_category_wise_expense_of_income = {}
     for i in expense_category:
         expense = Expense.objects.filter(profile=profile, time_stamp__month=month, time_stamp__year=year, category=i).aggregate(Sum('amount'))['amount__sum']
         expense_list = Expense.objects.filter(profile=profile, time_stamp__month=month, time_stamp__year=year, category=i)
         monthly_category_wise_expense[i.name] = expense
         if expense!=None:
             monthly_category_wise_expense_percent[i.name] = round((expense / monthly_expense)*100, 2)
+            monthly_category_wise_expense_of_income[i.name] = round((expense / monthly_income)*100, 2)
     template = 'finances/dashboard.html'
     context = {
         'month': month,
@@ -139,8 +140,54 @@ def DashboardView(request, username=None):
         'income_list': income_list,
         'expense_list': expense_list,
         'income_category': income_category,
-        'expense_category': expense_category
+        'expense_category': expense_category,
+        'monthly_category_wise_expense_of_income': monthly_category_wise_expense_of_income,
+        'expense_percent': expense_percent,
+        'saving_percent': saving_percent,
+        'monthly_income_details': monthly_income_details,
+        'monthly_expense_details': monthly_expense_details
     }
     return render(request, template, context)
 
 ####################################################################
+
+def ExpenseView(request, username=None):
+    context ={}
+    profile = User.objects.get(username=username)
+    category = ExpenseCategory.objects.filter(profile__username=username)
+    if 'month' in request.GET and 'year' in request.GET and 'category' in request.GET:
+       month = request.GET['month']
+       year = request.GET['year'] 
+       category = request.GET['category']
+       if month=='' and year=='' and category!=None:
+           context["expense_list"] = Expense.objects.filter(profile=profile, category=category)
+       elif month!=None and year!=None and category=='':
+           context["expense_list"] = Expense.objects.filter(profile=profile, time_stamp__month=month, time_stamp__year=year)  
+       elif month!=None and year=='' and category:
+           return HttpResponse('provide year')
+       elif month=='' and year!=None and category:
+           return HttpResponse('provide month')
+       elif month!=None and year!=None and category!=None:
+           context["expense_list"] = Expense.objects.filter(profile=profile, time_stamp__month=month, time_stamp__year=year, category=category)  
+
+    # elif 'month' in request.GET and 'year':
+    #     month = request.GET['month']
+    #     year = request.GET['year'] 
+    #     context["expense_list"] = Expense.objects.filter(profile=profile, time_stamp__month=month, time_stamp__year=year)    
+
+    # elif 'category' in request.GET:
+    #     category = request.GET['category'] 
+    #     context["expense_list"] = Expense.objects.filter(profile=profile, categoty=category)
+
+    else: 
+        context["expense_list"] = Expense.objects.filter(profile=profile) 
+    context["category_list"] = category
+    return render(request, "finances/expense_view.html", context)
+
+####################################################################
+
+def IncomeView(request, username=None):
+    context ={}
+    profile = User.objects.get(username=username)
+    context["income_list"] = Income.objects.filter(profile=profile)   
+    return render(request, "finances/income_view.html", context)
